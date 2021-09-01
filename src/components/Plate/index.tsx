f7c666ba3105ed7486aee796d08bbf77135843b9
Sávio Palácio Fontes <saviopf22@gmail.com>
2021-09-01 00:06:54 -0300

    primeira animação com imagem e testes

--- src/components/Plate/index.tsx
+++ src/components/Plate/index.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React from "react";
+import { MotiImage } from "moti";
 
-import { styles } from './styles';
-import plateImg from '../../assets/plate.png';
+import { styles } from "./styles";
+import plateImg from "../../assets/plate.png";
 
 export function Plate() {
   return (
-    <Image
+    <MotiImage
       style={styles.plate}
       source={plateImg}
       resizeMode="contain"
+      from={{ rotate: "90deg", opacity: 0 }}
+      // somente rotate '0' gira mais rapido
+      // animate={{ opacity: 1, rotate: "0" }}
+      animate={{ rotate: "0deg", opacity: 1 }}
+      transition={{
+        type: "timing",
+        duration: 2000,
+        // no caso deve ser número ímpar para terminar a animação
+        // com opacity 1
+        //repeat: 3,
+        // repetir para sempre
+        //loop: true,
+        // para não voltar de forma fluída a cada repetição
+        // repeatReverse: false,
+      }}
     />
   );
 }
