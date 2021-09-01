ea843a3e5c233d3a082c98eb228f7684de260812
Sávio Palácio Fontes <saviopf22@gmail.com>
2021-08-31 23:52:27 -0300

    adicionando animação no footer e header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import { Feather } from '@expo/vector-icons';
-import { View, Text } from 'react-native';
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { View, Text } from "react-native";
+import { MotiView } from "moti";
 
-import { styles } from './styles';
-import { theme } from '../../styles/theme';
+import { styles } from "./styles";
+import { theme } from "../../styles/theme";
 
 export function Header() {
   return (
-    <View style={styles.container}>
-      <Feather
-        name="arrow-left"
-        color={theme.colors.brown}
-        size={32}
-      />
+    <MotiView
+      from={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ type: "timing", duration: 1500 }}
+      style={styles.container}
+    >
+      <Feather name="arrow-left" color={theme.colors.brown} size={32} />
 
       <View>
-        <Text style={styles.title}>
-          Oriental Food
-        </Text>
+        <Text style={styles.title}>Oriental Food</Text>
 
-        <Text style={styles.subtitle}>
-          Special Sushi
-        </Text>
+        <Text style={styles.subtitle}>Special Sushi</Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
